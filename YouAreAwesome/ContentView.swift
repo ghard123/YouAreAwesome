abf8b01316aca1a848f1406ef268e7a2ae571995
Greg <ghard123@gmail.com>
2025-11-09 15:10:54 +1100

    First commit

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,14 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text("You are awesome!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
         }
         .padding()
     }
 }
+
+
+//comment
 
 #Preview {
     ContentView()
